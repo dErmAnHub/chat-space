9a8edfdeb2bc3c02ccdf409214d99ecce9fdb7f4
dErmAnHub <dtools001@gmail.com>
2020-07-18 21:10:41 +0900

    deploy.rb 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :rbenv_ruby,  '2.5.1'
 # set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
-set :ssh_options, auth_methods: ['publickey']
+set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/deploytest.pem']
 
 set :unicorn_config_path, -> {"#{current_path}/config/unicorn.rb"}
