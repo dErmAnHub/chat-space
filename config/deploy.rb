56f0c3891283f3e97f1656a9ce6c589b13436ca3
dErmAnHub <dtools001@gmail.com>
2020-07-19 12:55:16 +0900

    unicorn.rb  expand_path 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,4 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
-
-set :application, "chat-space"
-set :repo_url, "git@github.com:dErmAnHub/chat-space.git"
-set :linked_dirs, fetch(:linked_dirs. []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-set :rbenv_type, :user
-set :rbenv_ruby,  '2.5.1'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -35,8 +27,20 @@ set :rbenv_ruby,  '2.5.1'
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
+lock "3.14.1"
+
+set :application, "chat-space"
+
+set :repo_url, "git@github.com:dErmAnHub/chat-space.git"
+
+set :linked_dirs, fetch(:linked_dirs. []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby,  '2.5.1'
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> {"#{current_path}/config/unicorn.rb"}
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
@@ -44,8 +48,6 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/deploytest.pem']
-
-set :unicorn_config_path, -> {"#{current_path}/config/unicorn.rb"}
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
